3ce7a1cb5a23bcaa91cb2c1c45b1053d71223ca3
Ankit <work.ankitpokhrel@gmail.com>
2025-01-01 21:40:55 +0545

    day1

---
+++ Day_1/test.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
